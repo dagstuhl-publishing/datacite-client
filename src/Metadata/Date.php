bf303a3456441716fd81ee3f56e6a0702729b85a
didas <didas@dagstuhl.de>
2023-12-06 08:41:57 +0100

    added submitted and accepted dates

--- src/Metadata/Date.php
+++ src/Metadata/Date.php
@@ -4,48 +4,53 @@ namespace LZI\DataCite\Metadata;
 
 class Date
 {
-    private $date;
-    private $dateType;
-    private $dateInformation;
+    private string $date;
+    private string $dateType;
+    private ?string $dateInformation;
 
     protected function __construct(string $date, string $dateType, $dateInformation = NULL)
     {
         $this->date = $date;
         $this->dateType = $dateType;
         $this->dateInformation = $dateInformation;
-
     }
 
-    public static function issued(string $date, $dateInformation = NULL)
+    public static function issued(string $date, $dateInformation = NULL): self
     {
         return new static($date, 'Issued', $dateInformation);
     }
 
-    public static function available(string $date, $dateInformation = NULL)
+    public static function available(string $date, $dateInformation = NULL): self
     {
         return new static($date, 'Available', $dateInformation);
     }
 
-    public static function created(string $date, $dateInformation = NULL)
+    public static function created(string $date, $dateInformation = NULL): self
     {
         return new static($date, 'Created', $dateInformation);
     }
 
-    public static function copyrighted(string $date, $dateInformation = NULL)
+    public static function copyrighted(string $date, $dateInformation = NULL): self
     {
         return new static($date, 'Copyrighted', $dateInformation);
     }
 
+    public static function accepted(string $date, $dateInformation = NULL): self
+    {
+        return new static($date, 'Accepted', $dateInformation);
+    }
+
+    public static function submitted(string $date, $dateInformation = NULL): self
+    {
+        return new static($date, 'Submitted', $dateInformation);
+    }
+
     // TODO: add
-    // Accepted
     // Collected
-    // Submitted
     // Updated
     // Valid
     // Withdrawn
     // Other
-
-
 
     public function toApiObject()
     {
